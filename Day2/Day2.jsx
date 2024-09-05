499697586a1411ae9eeed48b5aa6cb8d87b2981e
Prithivi2625 <727723eucs172@skcet.ac.in>
2024-09-05 10:01:40 +0530

    Add files via upload

---
+++ Day2/Day2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Day2=()=>{
+    return (
+        <div className='Day2'>
+            <h1 style={{color:"green"}}>Inline Style with JSX example</h1>
+            <br></br>
+            <p>This is a paragraph with inline styles applied</p>
+        </div>
+    );
+};
+
+export default Day2;
